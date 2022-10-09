0b92dc266ca293614bcf6a7a77072bcb7daf1235
192114 <shadowSun192114@gmail.com>
2022-10-09 14:25:22 +0800

    root vscode settings

--- apps/spa/src/App.tsx
+++ apps/spa/src/App.tsx
@@ -8,7 +8,9 @@ const Login = lazy(() => import('./pages/Login'))
 function App() {
   return (
     <Suspense fallback={null}>
-      <div className={styles.title}>title<span>red</span></div>
+      <div className={styles.title}>
+        title<span>red</span>
+      </div>
       <Routes>
         <Route path="login" element={<Login />} />
       </Routes>
